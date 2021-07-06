60ba96b85b100b7678eba1553c3bf053ae62630f
Spacecat-Chan <49094338+SpaceCat-Chan@users.noreply.github.com>
2021-07-06 18:45:53 +0200

    dig_area attempt

---
+++ dig_area.lua
@@ -0,0 +1,112 @@
+local position = require("position")
+
+local arg = {...}
+if #arg < 3 then
+    print("Usage: x y z")
+end
+local x, y, z = tonumber(arg[1]), tonumber(arg[2]) tonumber(arg[3])
+if x == nil or y == nil or z == nil then
+    print("Usage: x y z")
+end
+
+if x == 0 or y == 0 or z == 0 then
+    return
+end
+x, y, z = x - 1, y - 1, z - 1
+
+local invert_x, invert_y, invert_z = x < 1, y < 1, z < 1
+
+local plus_x, minus_x, plus_y, minus_y, plus_z, minus_z = 1, 3, position.up, position.down, 2, 0
+if invert_x then
+    plus_x, minus_x = minus_x, plus_x
+end
+if invert_y then
+    plus_y, minus_y = minus_y, plus_y
+end
+if invert_z then
+    plus_z, minus_z = minus_z, plus_z
+end
+
+position.override(0, 1, 1, 1, "", "")
+
+
+function dig_x_line()
+    position.turn_to(plus_x)
+    while position.get_table()[1] ~= x do
+        turtle.dig()
+        position.forward()
+    end
+    position.turn_to(minus_x)
+    while position.get_table()[1] ~= 1 do
+        turtle.dig()
+        position.forward()
+    end
+end
+
+function dig_z_area()
+    local next_z
+    if not invert_z then
+        next_z = 2
+    else
+        next_z = 0
+    end
+    while position.get_table()[3] ~= z do
+        dig_x_line()
+        position.turn_to(plus_z)
+        while position.get_table()[3] ~= next_z do
+            turtle.dig()
+            position.forward()
+        end
+        if not invert_z then
+            next_z = next_z + 1
+        else
+            next_z = next_z - 1
+        end
+    end
+    position.turn_to(minus_z)
+    while position.get_table()[3] ~= 1 do
+        turtle.dig()
+        turtle.forward()
+    end
+end
+
+function dig_y_volume()
+    local next_y
+    if y < 1 then
+        next_y = 2
+    else
+        next_y = 0
+    end
+    while position.get_table()[2] ~= y do
+        dig_z_area()
+        while position.get_table()[2] ~= next_y do
+            if y < 1 then
+                turtle.digDown()
+            else
+                turtle.digUp()
+            end
+            plus_y()
+        end
+        if y < 1 then
+            next_y = next_y + 1
+        else
+            next_y = next_y - 1
+        end
+    end
+    while position.get_table()[2] ~= 1 do
+        if y < 1 then
+            turtle.digUp()
+        else
+            turtle.digDown()
+        end
+        minus_y()
+    end
+end
+
+while position.get_table()[1] ~= 1 do
+    turtle.dig()
+    position.forward()
+end
+dig_y_volume()
+position.turn_to(plus_x)
+position.back()
---
+++ position.lua
@@ -0,0 +1,174 @@
+--local JSON = require("JSON")
+local JSON = {}
+
+local x, y, z, o, dim, server
+
+local instance = {}
+
+function instance.override(new_x, new_y, new_z, new_o, new_dim, new_server)
+	x = new_x
+	y = new_y
+	z = new_z
+	o = new_o
+	dim = new_dim
+	server = new_server
+	instance.update_remote()
+end
+
+function instance.turnLeft()
+	o = o - 1
+	if o < 0 then o = o + 4 end
+	local result = turtle.turnLeft()
+	instance.update_remote()
+	return result
+end
+
+function instance.turnRight()
+	o = o + 1
+	if o > 3 then o = o - 4 end
+	local result = turtle.turnRight()
+	instance.update_remote()
+	return result
+end
+
+function instance.turn_to(target_o)
+    if target_o - o == 0 then
+        return
+    end
+    if target_o - o == -1 then
+        instance.turnRight()
+        return
+    end
+    if target_o - o == -2 then
+        instance.turnRight()
+        instance.turnRight()
+        return
+    end
+    if target_o - o == -3 then
+        instance.turnLeft()
+        return
+    end
+    if target_o - o == 1 then
+        instance.turnLeft()
+        return
+    end
+    if target_o - o == 2 then
+        instance.turnLeft()
+        instance.turnLeft()
+        return
+    end
+    if target_o - o == 3 then
+        instance.turnRight()
+        return
+    end
+end
+
+function instance.orientation_to_offset(_o)
+	local offset_x, offset_z
+	if _o == 0 then
+		offset_x = 0
+		offset_z = -1
+	elseif _o == 1 then
+		offset_x = 1
+		offset_z = 0
+	elseif _o == 2 then
+		offset_x = 0
+		offset_z = 1
+	elseif _o == 3 then
+		offset_x = -1
+		offset_z = 0
+	end
+	return offset_x, offset_z
+end
+
+function instance.forward()
+	local success = turtle.forward()
+	if success then
+		local offset_x, offset_z = instance.orientation_to_offset(o)
+		x, z = x + offset_x, z + offset_z
+		instance.update_remote()
+	end
+	return success
+end
+
+function instance.back()
+	local success = turtle.back()
+	if success then
+		local offset_x, offset_z = instance.orientation_to_offset(o)
+		x, z = x + -offset_x, z + -offset_z
+		instance.update_remote()
+	end
+	return success
+end
+
+function instance.up()
+	local success = turtle.up()
+	if success then
+		y = y + 1
+		instance.update_remote()
+	end
+	return success
+end
+
+function instance.down()
+	local success = turtle.down()
+	if success then
+		y = y - 1
+		instance.update_remote()
+	end
+	return success
+end
+
+function instance.get()
+	return x,y,z,o,dim,server
+end
+
+function instance.get_table()
+	return {instance.get()}
+end
+
+function instance.register_websocket(ws)
+	instance.ws = ws
+end
+
+function instance.update_remote()
+	if instance.ws then
+		local info = {
+			request_id = -2,
+			response = {
+				x = x,
+				y = y,
+				z = z,
+				o = o,
+				dimension = dim,
+				server = server
+			}
+		}
+		instance.ws.send(JSON:encode(info))
+		local blocks = execute_command({
+			request_type = "command buffer",
+			commands = {
+				{
+					request_type = "inspect",
+					direction = "forward"
+				},
+				{
+					request_type = "inspect",
+					direction = "up"
+				},
+				{
+					request_type = "inspect",
+					direction = "down"
+				}
+			}
+		})
+		local response = {
+			request_id = -1,
+			response = blocks
+		}
+		local json_response = JSON:encode(response)
+		instance.ws.send(json_response)
+	end
+end
+
+return instance
