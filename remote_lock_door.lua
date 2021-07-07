5d2919fb7527526b371f5b65cc97475e07937b21
Spacecat-Chan <49094338+SpaceCat-Chan@users.noreply.github.com>
2021-07-08 01:37:40 +0200

    update to back

--- remote_lock_door.lua
+++ remote_lock_door.lua
@@ -4,9 +4,9 @@ modem.open(6780)
 local verify_token = "0x40 0x41 0x42"
 
 function open_door()
-    redstone.setOutput("front", true)
+    redstone.setOutput("back", true)
     sleep(10)
-    redstone.setOutput("front", false)
+    redstone.setOutput("back", false)
 end
 
 while true do
