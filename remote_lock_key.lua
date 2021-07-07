2c096f4ab327847d59d1cd6e82aea6149e15ccdb
Spacecat-Chan <49094338+SpaceCat-Chan@users.noreply.github.com>
2021-07-08 01:25:14 +0200

    test

--- remote_lock_key.lua
+++ remote_lock_key.lua
@@ -1,5 +1,8 @@
 local modems = {peripheral.find("modem")}
 local modem = modems[1]
 if modem then
+    print("found modem")
     modem.transmit(6780, 6781, "0x40 0x41 0x42")
+else
+    print("did not find modem")
 end
