7782d84aea8a2e13c40b46e69b94cf338c69cbfd
SpaceCat-Chan <49094338+SpaceCat-Chan@users.noreply.github.com>
2021-07-19 13:27:32 +0200

    settle on a function based style

--- cfa/examples/basic.lua
+++ cfa/examples/basic.lua
@@ -5,18 +5,30 @@ local b = cfa.var(" world!")
 
 cfa.call(print, a..b)
 
-local add = cfa.func("add_numbers")
-local first = add.arg()
-local second = add.arg()
-local result = first + second
-add.result(result)
+local add = cfa.func(function(cfa)
+    return cfa.arg() + cfa.arg()
+end)
 
 cfa.call(print, add(2, 2))
 
 local number = cfa.var(0)
-local while_not_10 = cfa.while_(number < cfa.var(10))
-while_not_10.call(print, number)
-while_not_10.assign(number, number + 1)
+cfa.while_(number < 10, function(cfa)
+    cfa.call(print, number)
+    number.a = number + 1
+end)
+
+local complex_function = cfa.func(function(cfa)
+    cfa.if_(cfa.arg() > 10, function(cfa)
+        return cfa.null
+        -- cfa.null returns from the outer function
+    end, function(cfa)
+        -- nil return is treated as the if statement not doing anything special
+        return nil
+    end)
+    cfa.call(print, cfa.arg())
+end)
+
+complex_function(11, "not printed")
+complex_function(9, "printed")
 
 cfa.run("basic_example")
-
